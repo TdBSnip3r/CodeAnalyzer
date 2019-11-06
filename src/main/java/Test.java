20ffc5d89031555b25bb2c99360c68a5925d1bfe
_Retro_ <39262705+TdBSnip3r@users.noreply.github.com>
2019-11-06 16:34:43 +0100

    Primo commit su Clonigrepo
    Cosa è stato fatto:
    Nel file build.gradle è stata aggiunta una sezione a JGit, libreria fornente API per utilizzare Git tramite codice Java. E' stato aggiunto anche un altro testCompile denominato org.slf4j che serviva per evitare un messaggio di Log nella console esprimento un ERROR SLF4J
    E' stata creata la classe controllore InitController definita con pattern Singleton. Questo controllore al momento incapsula 2 funzionalità importanti quali il clone di un repository remoto in uno specifico path del nostro File System. L'operazione deleteRepo per eliminare il Repository appena scaricato. Contiene anche altre funzioni private al momento non inerenti le specifiche del commit.
    Nella classe Test src/main ho aggiunto i riferimenti all'utilizzo di InitController.
    A breve ci sarà un commit con la documentazione UML definita.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,23 @@
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.TransportException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class Test {
-    public static void main(String args[])
-    {
-        System.out.println("Piccolo test CI");
+    public static void main(String args[]){
+        String repoUrl = "https://github.com/TdBSnip3r/TEST.git";
+        String clonepathDir = "C:\\Users\\vince\\Desktop\\Repo";
+        InitController.getInstance().setRepoUrl(repoUrl);
+        InitController.getInstance().setClonepathDir(clonepathDir);
+        boolean done = InitController.getInstance().jgitCloneRepository();
+        //boolean delete = InitController.getInstance().jgitDeleteRepository();
     }
+
+
+
 }
