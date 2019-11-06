c4d8eed49d2bd66a28385aaebc5aa7527e241dfe
_Retro_ <39262705+TdBSnip3r@users.noreply.github.com>
2019-11-06 16:46:39 +0100

    Ho creato l'involucro dei casi di Test
    Qui ho creato l'involucro della classe Junit Test relativa  a InitController e piu in particolare alle 2 funzioni cardine e principali quali jgitCloneRepository e jgitDeleteRepository. Il testing non è stato ancora implementato

---
+++ src/test/java/InitControllerTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+
+public class InitControllerTest {
+
+
+    ArrayList<String> clonePathDirs = new ArrayList<String>();
+    ArrayList<String> repoUrls = new ArrayList<String>();
+    @Test
+    public void jgitCloneRepository() {
+
+    }
+
+    @Test
+    public void jgitDeleteRepository() {
+        
+    }
+}
